8a5a3db1dce5e70a5c7c48cf35d4729e9cdd0786
HellLine <165831538+HellLine@users.noreply.github.com>
2024-04-02 23:51:27 +0530

    pagerankalgorithm.py

--- pagerankalgorithm.py
+++ pagerankalgorithm.py
@@ -1,19 +1,20 @@
-import numpy as n
-from fractions import Fraction as f
-dp = f(1,3)
-m = n.matrix([[0,0,1],[f(1,2),0,0],[f(1,2),1,0]])
-ex = n.zeros((3,3))
+import numpy as np
+from fractions import Fraction as F
+
+dp = F(1, 3)
+m = np.matrix([[0, 0, 1], [F(1, 2), 0, 0], [F(1, 2), 1, 0]])
+ex = np.zeros((3, 3))
 ex[:] = dp
 b = 0.7
-a= b * m + ((1-b) * ex)
-r = n.matrix([dp, dp, dp]).transpose()
+a = b * m + ((1 - b) * ex)
+r = np.matrix([dp, dp, dp]).transpose()
 prev_r = r
-for i in range(1,100):
-r = a*r
-res=n.round((r).astype(float), decimals=3)
-print (res)
-if (prev_r==r).all():
-break
-prev_r = r
-print ("Final:", res)
-print ("sum", n.sum(r))
+for i in range(1, 100):
+    r = a * r
+    res = np.round((r).astype(float), decimals=3)
+    print(res)
+    if (prev_r == r).all():
+        break
+    prev_r = r
+print("Final:", res)
+print("Sum", np.sum(r))
